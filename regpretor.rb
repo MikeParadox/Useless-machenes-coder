6f2bb5a683c006243c149163ba628727ba36d8d3
Mikhail <hudih@mail.ru>
2025-05-28 19:52:53 +0300

    added human readable representation of machine code

--- regpretor.rb
+++ regpretor.rb
@@ -1,6 +1,44 @@
 # frozen_string_literal: true
 
 class Regpretor
+  def convert_code_to_instuctions(filepath)
+    result = []
+    File.readlines(filepath).map do |line|
+      values = line.chomp.split(',')
+      result << values
+    end
+    newline = RUBY_PLATFORM =~ /win32|win64|mswin|mingw/ ? "\r\n" : "\n"
+    reg_machine = ""
+    result.each_entry do |line|
+      command = ""
+      if line.length == 2
+        command += line[1] + ": stop"
+      else
+        if line.length < 5
+          command += line[1].strip + ": x" + line[2].strip
+          command = command.strip
+          if line[0] == "1"
+            command += "<-" + line[3].strip
+          elsif line[0] == "2"
+            command += "<-x" + line[2].strip + "+1"
+          elsif line[0] == "3"
+            command += "x<-" + line[2].strip + "-1"
+          end
+        else
+          command += line[1] + ": if x" + line[2] + "=0 goto " +
+                     line[3] + " else " + line[4]
+        end
+      end
+      reg_machine += command + newline
+    end
+
+    puts reg_machine
+    File.open("machine_code.txt", 'w') do |file|
+      reg_machine.each_line { |line| file.puts(line.chomp) }
+
+    end
+  end
+
   def interpret(filepath)
     lines = File.readlines(filepath)
 
@@ -12,7 +50,6 @@ class Regpretor
       conv = convert(line)
       rb.write(conv)
     end
-
 
     rb.write("puts l1")
     rb.close
@@ -28,36 +65,36 @@ class Regpretor
   end
 
   def convert(line)
-    codes = line.split(',').map {|code| code.to_i}
-    #codes = line_split.each do |code| code.to_i end
+    codes = line.split(',').map { |code| code.to_i }
+    # codes = line_split.each do |code| code.to_i end
     case codes[0]
-      when 1 then return %{
+    when 1 then return %{
       def l#{codes[1]}()
-        $x[#{codes[2]-1}] = #{codes[3]}
-        l#{codes[1]+1}
+        $x[#{codes[2] - 1}] = #{codes[3]}
+        l#{codes[1] + 1}
       end
       }
-      when 2 then return %{
+    when 2 then return %{
       def l#{codes[1]}()
-        $x[#{codes[2]-1}] += 1
-        l#{codes[1]+1}
+        $x[#{codes[2] - 1}] += 1
+        l#{codes[1] + 1}
       end
       }
-     when 3 then puts %{
+    when 3 then puts %{
       def l#{codes[1]}()
-        $x[#{codes[2]-1}] -= 1
-        l#{codes[1]+1}
+        $x[#{codes[2] - 1}] -= 1
+        l#{codes[1] + 1}
       end
       }
-     when 4 then return %{
+    when 4 then return %{
       def l#{codes[1]}()
-        if $x[#{codes[2]-1}]== 0
+        if $x[#{codes[2] - 1}]== 0
         l#{codes[3]}
         else l#{codes[4]}
         end
       end
       }
-      when 5 then return %{
+    when 5 then return %{
       def l#{codes[1]}()
         puts $x[0]
         exit
@@ -68,4 +105,4 @@ class Regpretor
   end
 end
 
-Regpretor.new.interpret("test.txt")
+Regpretor.new.convert_code_to_instuctions("test.txt")
