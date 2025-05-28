ae1f55544d54595698b29c2cbacd267e7c95b3fc
MikeParadox <71334408+MikeParadox@users.noreply.github.com>
2025-05-28 23:49:31 +0300

    Revert "added human readable representation of machine code"

--- regpretor.rb
+++ regpretor.rb
@@ -1,44 +1,6 @@
 # frozen_string_literal: true
 
 class Regpretor
-  def convert_code_to_instuctions(filepath)
-    result = []
-    File.readlines(filepath).map do |line|
-      values = line.chomp.split(',')
-      result << values
-    end
-    newline = RUBY_PLATFORM =~ /win32|win64|mswin|mingw/ ? "\r\n" : "\n"
-    reg_machine = ""
-    result.each_entry do |line|
-      command = ""
-      if line.length == 2
-        command += line[1] + ": stop"
-      else
-        if line.length < 5
-          command += line[1].strip + ": x" + line[2].strip
-          command = command.strip
-          if line[0] == "1"
-            command += "<-" + line[3].strip
-          elsif line[0] == "2"
-            command += "<-x" + line[2].strip + "+1"
-          elsif line[0] == "3"
-            command += "x<-" + line[2].strip + "-1"
-          end
-        else
-          command += line[1] + ": if x" + line[2] + "=0 goto " +
-                     line[3] + " else " + line[4]
-        end
-      end
-      reg_machine += command + newline
-    end
-
-    puts reg_machine
-    File.open("machine_code.txt", 'w') do |file|
-      reg_machine.each_line { |line| file.puts(line.chomp) }
-
-    end
-  end
-
 
   def run(filepath,*regs)
     rb = File.open(filepath, "a") do |file|
@@ -51,7 +13,6 @@ class Regpretor
     output = `ruby #{filepath}`
     puts "Output from #{filepath}:\n#{output}"
   end
-
   def interpret(filepath)
     lines = File.readlines(filepath)
     name = File.basename(filepath, ".*")
@@ -64,7 +25,8 @@ class Regpretor
       rb.write(conv)
     end
 
-    rb.write("puts l1")
+
+    #rb.write("puts l1")
     rb.close
     File.basename(rb)
   end
@@ -77,36 +39,36 @@ class Regpretor
   end
 
   def convert(line)
-    codes = line.split(',').map { |code| code.to_i }
-    # codes = line_split.each do |code| code.to_i end
+    codes = line.split(',').map {|code| code.to_i}
+    #codes = line_split.each do |code| code.to_i end
     case codes[0]
-    when 1 then return %{
+      when 1 then return %{
       def l#{codes[1]}()
-        $x[#{codes[2] - 1}] = #{codes[3]}
-        l#{codes[1] + 1}
+        $x[#{codes[2]-1}] = #{codes[3]}
+        l#{codes[1]+1}
       end
       }
-    when 2 then return %{
+      when 2 then return %{
       def l#{codes[1]}()
-        $x[#{codes[2] - 1}] += 1
-        l#{codes[1] + 1}
+        $x[#{codes[2]-1}] += 1
+        l#{codes[1]+1}
       end
       }
-    when 3 then puts %{
+     when 3 then puts %{
       def l#{codes[1]}()
-        $x[#{codes[2] - 1}] -= 1
-        l#{codes[1] + 1}
+        $x[#{codes[2]-1}] -= 1
+        l#{codes[1]+1}
       end
       }
-    when 4 then return %{
+     when 4 then return %{
       def l#{codes[1]}()
-        if $x[#{codes[2] - 1}]== 0
+        if $x[#{codes[2]-1}]== 0
         l#{codes[3]}
         else l#{codes[4]}
         end
       end
       }
-    when 5 then return %{
+      when 5 then return %{
       def l#{codes[1]}()
         puts $x[0]
         exit
@@ -117,6 +79,5 @@ class Regpretor
   end
 end
 
-Regpretor.new.convert_code_to_instuctions("test.txt")
 Regpretor.new.interpret("test.txt")
 Regpretor.new.run("test.rb",[0])
